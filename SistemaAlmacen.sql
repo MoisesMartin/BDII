b88aee3fad3b6fa7c8a45f999399ac971aed79c4
MoisesMartin <31869894+MoisesMartin@users.noreply.github.com>
2018-01-31 20:33:46 -0600

    Add files via upload

---
+++ SistemaAlmacen.sql
@@ -0,0 +1,14 @@
+create database Moises
+use database Moises
+create table Almacen(numero_almacen integer, ubicacion_almacen varchar2(40),constraint pk_na primary key(numero_almacen));
+
+--procedimiento almacenado
+create or replace procedure guardar_almacen(my_numero_almacen in integer, my_ubicacion_almacen in varchar2)
+as
+begin
+insert into Almacen values (my_numero_almacen, my_ubicacion);
+end;
+/
+--select para verificar que se guardo 
+select * from Almacen;
+--
